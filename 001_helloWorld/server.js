f86ade41a82ec04068ce7b0d0e056c75487d14d0
Cyrille YAO <cyaoyapi@gmail.com>
2019-02-01 23:35:26 +0000

    Return valid HTML

--- 001_helloWorld/server.js
+++ 001_helloWorld/server.js
@@ -2,7 +2,18 @@ let http = require('http'); // A HTTP object
 // A HTTP Server(web) creation 
 let server = http.createServer(function(request, response){
 	response.writeHead(200, {"Content-Type": "text/html"});
-	response.end("<p>Hello world!<p><p>You're <strong>welcome</strong>!</p>");
+	response.write('<!DOCTYPE html>'+
+		'<html>'+
+			'<head>'+
+				'<meta charset="utf-8">'+
+				'<title>My first page with Node.js</title>'+
+			'</head>'+
+			'<body>'+
+				"<p>Hello world!<p><p>You're <strong>welcome</strong>!</p>"+
+			'</body>'+
+		'</html>'
+	);
+	response.end();
 });
 
 server.listen(8070); // The server wil listen on port 8070
