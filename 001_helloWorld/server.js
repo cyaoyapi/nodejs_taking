fc482ea84a4bb863fb52e3174fe7c7b642144956
Cyrille YAO <cyaoyapi@gmail.com>
2019-02-01 23:46:03 +0000

    Recover the url

--- 001_helloWorld/server.js
+++ 001_helloWorld/server.js
@@ -1,17 +1,22 @@
-let http = require('http'); // A HTTP object
+let http = require('http'); 
+let url = require('url'); 
+
 // A HTTP Server(web) creation 
 let server = http.createServer(function(request, response){
+	let page = url.parse(request.url).pathname;
 	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write('<!DOCTYPE html>'+
-		'<html>'+
-			'<head>'+
-				'<meta charset="utf-8">'+
-				'<title>My first page with Node.js</title>'+
-			'</head>'+
-			'<body>'+
-				"<p>Hello world!<p><p>You're <strong>welcome</strong>!</p>"+
-			'</body>'+
-		'</html>'
+	response.write(`<!DOCTYPE html>
+		<html>
+			<head>
+				<meta charset="utf-8">
+				<title>My first page with Node.js</title>
+			</head>
+			<body>è
+				<p>Hello world!<p>
+				<p>You're <strong>welcome</strong>!</p>
+				<p>The request come from : <em>${page}</em></p>
+			</body>
+		</html>`
 	);
 	response.end();
 });
