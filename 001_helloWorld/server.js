5d792a861405762a7e9140a6ffdfa13c01687617
Cyrille YAO <cyaoyapi@gmail.com>
2019-02-01 23:25:03 +0000

    The server returns HTML

--- 001_helloWorld/server.js
+++ 001_helloWorld/server.js
@@ -1,8 +1,8 @@
 let http = require('http'); // A HTTP object
 // A HTTP Server(web) creation 
 let server = http.createServer(function(request, response){
-	response.writeHead(200);
-	response.end("Hello world! You're welcome!");
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.end("<p>Hello world!<p><p>You're <strong>welcome</strong>!</p>");
 });
 
 server.listen(8070); // The server wil listen on port 8070
